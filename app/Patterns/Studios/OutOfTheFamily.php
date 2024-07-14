6b3af5c87b52357894c974e4bf2f838c40d10d95
bjorn <bjorn.sodergren@gmail.com>
2024-07-14 04:49:38 -0500

    up

---
+++ app/Patterns/Studios/OutOfTheFamily.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Command like Metatag writer for video files.
+ */
+
+namespace Mediatag\Patterns\Studios;
+
+use Mediatag\Patterns\Studios\DevilsFilm;
+
+class OutOfTheFamily extends DevilsFilm
+{
+    public $subStudio = 'Out Of The Family';
+}
