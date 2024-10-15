1b5fb8cf4ffef4caafdb58018b4860c015452dbc
bjorn <bjorn.sodergren@gmail.com>
2024-10-15 21:01:32 +0000

    n

---
+++ app/Patterns/Studios/Spizoo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Command like Metatag writer for video files.
+ *
+ */
+
+namespace Mediatag\Patterns\Studios;
+use Mediatag\Modules\TagBuilder\Patterns;
+
+ 
+
+const SPIZOO_REGEX_COMMON = '//i';
+
+class Spizoo extends Patterns
+{
+
+    public $studio = 'Spizoo';
+     
+
+}
