aae6fed53b17938885c0a57ec2648c9dd13229e6
bjorn <bjorn.sodergren@gmail.com>
2024-11-07 03:21:52 +0000

    new

---
+++ app/Patterns/Studios/PinkoClub.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Command like Metatag writer for video files.
+ *
+ */
+
+namespace Mediatag\Patterns\Studios;
+use Mediatag\Modules\TagBuilder\Patterns;
+
+ 
+
+const PINKOCLUB_REGEX_COMMON = '//i';
+
+class PinkoClub extends Patterns
+{
+
+    public $studio = 'Pinko Club';
+     
+
+}
