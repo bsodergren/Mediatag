b37889761955fd8e8fae4e784eab3c21d84b0abb
bjorn <bjorn.sodergren@gmail.com>
2025-02-02 06:58:47 +0000

    p

---
+++ app/Patterns/Studios/UpClose.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Command like Metatag writer for video files.
+ *
+ */
+
+namespace Mediatag\Patterns\Studios;
+use Mediatag\Modules\TagBuilder\Patterns;
+
+ 
+
+const UPCLOSE_REGEX_COMMON = '//i';
+
+class UpClose extends AdultTime
+{
+
+    public $studio = 'Up Close';
+    public $network  = 'Adult Time';
+     
+
+}
