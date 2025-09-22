6c734984c75e183fd0e1d089721f4eb1c18391e9
Bsodergren <bjorn.sodergren@gmail.com>
2025-09-22 07:33:30 +0000

    bb

--- bin/procVideos.sh
+++ bin/procVideos.sh
@@ -7,6 +7,9 @@ PORNHUB_DIR="${PLEX_HOME}/Pornhub/"
 PREMIUM_DIR="${PORNHUB_DIR}/Premium/"
 SORT_DIR="${PORNHUB_DIR}/Sort/"
 
+__FILE="list.txt"
+__MAX=5
+
 function print_usage() {
    __genre_list_str=$(printf ",%s" "${__GENRE_LIST[@]}")
    __genre_list_str=${__genre_list_str:1}
@@ -19,7 +22,9 @@ Options:
    -s,  --sort       Run Mediaupdate in sort folder
    -m,  --move       Move downloaded files, update and sort
    -u,  --update     Run Media DB
-"
+   -f, --file
+   "
+
    echo "$__usage"
 
 }
@@ -28,9 +33,11 @@ for arg in "$@"; do
    shift
    case "$arg" in
    "--playlist") set -- "$@" "-p" ;;
+   "--max") set -- "$@" "-M" ;;
    "--sort") set -- "$@" "-s" ;;
    "--move") set -- "$@" "-m" ;;
    "--update") set -- "$@" "-u" ;;
+   "--file") set -- "$@" "-f" ;;
    *) set -- "$@" "$arg" ;;
    esac
 done
@@ -38,13 +45,15 @@ done
 # Parse short options
 OPTIND=1
 
-while getopts "psmu" opt; do
+while getopts "psmuM:f:" opt; do
 
    case "$opt" in
    "p") __PLAYLIST=1 ;;
    "s") __SORT=1 ;;
    "m") __MOVE=1 ;;
    "u") __MEDIADB=1 ;;
+   "M") __MAX=$OPTARG ;;
+   "f") __FILE=$OPTARG ;;
    *)
       print_usage >&2
       exit 1
@@ -55,14 +64,19 @@ shift $((OPTIND - 1))
 
 shopt -s nocasematch
 
-if [[ -n "${__PLAYLIST}" ]]; then
-echo "Running Playlst CMD"
+function DoPlaylist() {
+    echo "The name of this function is: ${FUNCNAME[0]}"
    cd "${PLAYLIST_DIR}"
-   playlist pl -M5 list.txt
+   playlist pl -M${__MAX} ${__FILE}
+
+}
+
+if [[ -n "${__PLAYLIST}" ]]; then
+   DoPlaylist
 fi
 
 if [[ -n "${__MOVE}" ]]; then
-echo "Running Move CMD"
+   echo "Running Move CMD"
    cd "${DOWNLOAD_DIR}"
    mediadownload
    cd "${PREMIUM_DIR}"
@@ -70,8 +84,12 @@ echo "Running Move CMD"
    mediarename move -g
 fi
 
+if [[ -n "${__PLAYLIST}" ]] || [[ -n "${__MOVE}" ]]; then
+   exit
+fi
+
 if [[ -n "${__SORT}" ]]; then
-echo "Running Sort CMD"
+   echo "Running Sort CMD"
    cd "${SORT_DIR}"
    pwd
    mediaupdate
@@ -79,7 +97,7 @@ echo "Running Sort CMD"
 fi
 
 if [[ -n "${__MEDIADB}" ]]; then
-echo "Running MedaDB CMD"
+   echo "Running MedaDB CMD"
    cd "${PORNHUB_DIR}"
    mediadb
    mediadb all
