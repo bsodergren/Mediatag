8d1b473b4e58c1c89488d392953498edd7ca995f
bsodergren <bjorn.sodergren@gmail.com>
2026-02-08 08:10:39 -0600

    x

--- app/Traits/DynamicProperty.php
+++ app/Traits/DynamicProperty.php
@@ -8,18 +8,18 @@ use ReflectionProperty;
 
 trait DynamicProperty
 {
-    private array $data = [];
+    private array $DynamicData = [];
 
     public function __set($name, $value): void
     {
-        $prefix                     = Strings::after(__CLASS__, '\\', -1);
-        $this->data[$prefix][$name] = $value;
+        $prefix                            = Strings::after(__CLASS__, '\\', -1);
+        $this->DynamicData[$prefix][$name] = $value;
     }
 
     public function __get(string $name)
     {
         $prefix = Strings::after(__CLASS__, '\\', -1);
 
-        return $this->data[$prefix][$name] ?? null;
+        return $this->DynamicData[$prefix][$name] ?? null;
     }
 }
